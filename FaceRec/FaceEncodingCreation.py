2ab6bdf735642731e81001e3748c067aa0f6342b
felipe <felipe.pereira.montero@gmail.com>
2022-08-31 12:24:05 -0300

    Loop de reconocimiento facial

--- FaceRec/FaceEncodingCreation.py
+++ FaceRec/FaceEncodingCreation.py
@@ -17,14 +17,20 @@ def main():
 
     # Genera los encodings y los guarda en una carpeta
     for image_path, image_name in zip(photo_list, photo_name):
-        tmp_img = face_recognition.load_image_file(image_path)
-        tmp_location = face_recognition.face_locations(tmp_img)
-        tmp_img_encoding = face_recognition.face_encodings(tmp_img, tmp_location)[0]
-        name_no_ext = os.path.splitext(image_name)[0]
-        if not os.path.exists(f"/home/felipe/FaceEncodings/{name_no_ext}"):
-            os.mkdir(f"/home/felipe/FaceEncodings/{name_no_ext}")
-        print(f"/home/felipe/FaceEncodings/{name_no_ext}")
-        numpy.save(f"/home/felipe/FaceEncodings/{name_no_ext}/encoding{name_no_ext}", tmp_img_encoding)
+        # Carga la imgaen
+        img = face_recognition.load_image_file(image_path)
+        locations = face_recognition.face_locations(img)
+        # Si encuentra caras en la imagen guarda los encodings
+        # Si no encuentra menciona el nombre del que dio error
+        if locations:
+            tmp_img_encoding = face_recognition.face_encodings(img, locations)[0]
+            name_no_ext = os.path.splitext(image_name)[0]
+            if not os.path.exists(f"/home/felipe/FaceEncodings/{name_no_ext}"):
+                os.mkdir(f"/home/felipe/FaceEncodings/{name_no_ext}")
+            print(f"/home/felipe/FaceEncodings/{name_no_ext}")
+            numpy.save(f"/home/felipe/FaceEncodings/{name_no_ext}/encoding{name_no_ext}", tmp_img_encoding)
+        else:
+            print(f"Error con {image_name}")
 
 
 if __name__ == '__main__':
--- FaceRec/FaceRecognition.py
+++ FaceRec/FaceRecognition.py
@@ -1,37 +1,77 @@
 import face_recognition
 from DB.Db import mdbconn
 import numpy as np
+import cv2
+import time
 
 
-def face_authentication(img: np.ndarray) -> str:
+def face_authentication(img_enc) -> tuple:
     """Authenticate given image (ndarray) with the database and return name of the authenticated person.
-    Always returns something.
+    Return None if no match found.
     """
-    new_img_encoding = face_recognition.face_encodings(img)[0]
-
+    # Genera encoding para la foto recibida
+    # new_img_encoding = face_recognition.face_encodings(img, face_locations)[0]
+    # Abre la conexion con la base de datos
     conn = mdbconn()
     cur = conn.cursor()
     sql_query = "SELECT EncodingPath FROM Players"
     cur.execute(sql_query)
+    # Lista de encodings del tipo nparray
     encodings_list = []
+    # Lista de paths a los encodings para sacar el nombre
     encodings_path_list = []
+    # Saca todo EncodingPath de la base y lo carga con numpy en una lista de encodings
     for encoding_path in cur:
         # encoding_path[0] porque sale como tuple
         encodings_path_list.append(encoding_path[0])
         encoding = np.load(encoding_path[0])
         encodings_list.append(encoding)
-    face_distances = face_recognition.face_distance(encodings_list, new_img_encoding)
+    # Selecciona el encoding con la minima distancia
+    face_distances = face_recognition.face_distance(encodings_list, img_enc)
     best_match_index = np.argmin(face_distances)
-    match = encodings_path_list[best_match_index]
-    split_path = match.rsplit("/")
-    match_name_location = split_path.index("FaceEncodings") + 1
-    match_name = split_path[match_name_location]
-    return match_name
+    # Si la distancia es menor a 0.6 retorna el nombre
+    # Si es mayor retorna None
+    if face_distances[best_match_index] > 0.6:
+        return "", 0
+    else:
+        # El indice de la distancia corresponde al indice de su path, con el cual se saca el nombre
+        match = encodings_path_list[best_match_index]
+        split_path = match.rsplit("/")
+        match_name_location = split_path.index("FaceEncodings") + 1
+        match_name = split_path[match_name_location]
+        print(face_distances[best_match_index])
+        return match_name, face_distances[best_match_index]
 
 
 def main():
-    pinie = face_recognition.load_image_file("/home/felipe/PinballFaces/Riki/Riki.jpg")
-    face_authentication(pinie)
+    video_capture = cv2.VideoCapture(0)
+    prev = 0
+    frame_rate = 2
+    while True:
+        time_elapsed = time.time() - prev
+        ret, frame = video_capture.read()
+
+        if time_elapsed > 1 / frame_rate:
+            prev = time.time()
+            small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+
+            rgb_small_frame = cv2.cvtColor(small_frame, cv2.COLOR_BGR2RGB)
+            face_locations = face_recognition.face_locations(rgb_small_frame)
+            if face_locations:
+                # Autentica la imagen con las referencias
+                image_encoding = face_recognition.face_encodings(rgb_small_frame, face_locations)[0]
+                match, distance = face_authentication(image_encoding)
+                if match != "":
+                    print(match)
+                    print(distance)
+        cv2.imshow("Video", small_frame)
+
+        c = cv2.waitKey(1)
+        if c == 27:
+            break
+
+    video_capture.release()
+    cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
